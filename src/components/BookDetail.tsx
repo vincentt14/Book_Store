0e49fdeeb72a33a005f0a56f27fad127a6c04fa2
Vincent <vincent14@student.umn.ac.id>
2023-02-23 13:48:58 +0700

    adding detail page

--- src/components/BookDetail.tsx
+++ src/components/BookDetail.tsx
@@ -1,7 +1,44 @@
-const BookDetail = () => {
-  return (
-    <h1> testt</h1>
-  )
+import { useState } from "react";
+import postedAt from "../utils/postedAt";
+
+interface IBookDetail {
+  title: string;
+  author: string;
+  description: string;
+  postDate: string;
+  saved: boolean;
 }
+
+const BookDetail: React.FC<IBookDetail> = ({ title, author, description, postDate, saved }) => {
+  const [save, setSave] = useState(saved);
+
+  const onSaveChange = (e: any) => {
+    e.preventDefault();
+    setSave(!save);
+  };
+
+  return (
+    <>
+      <h3 className="font-semibold text-secondary text-xl mt-5 mb-3">
+        <p className="cursor-pointer hover:text-primary">{title}</p>
+      </h3>
+      <p className="font-medium font-mono text-base text-slate-500">
+        Author : <span className="text-primary">{author}</span>
+      </p>
+      <p className="font-medium font-mono text-base text-slate-500 mb-2">
+        Posted : <span className="text-primary">{postedAt(postDate)}</span>
+      </p>
+      <p className="font-medium font-mono text-base text-slate-500 mb-2">{description}</p>
+      <button
+        className={
+          save === false
+            ? "rounded-sm btnn bg-black hover:bg-white text-white hover:text-black border-black border-2 py-2 px-6 ease-out duration-300 focus:outline-none focus:ring focus:ring-blue-500 mb-16 lg:mb-20"
+            : "rounded-sm btnn bg-white hover:bg-white text-black hover:text-black border-black border-2 py-2 px-6 ease-out duration-300 focus:outline-none focus:ring focus:ring-blue-500 mb-16 lg:mb-20"
+        }
+        onClick={onSaveChange}
+      ></button>
+    </>
+  );
+};
 
 export default BookDetail;
--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,15 +1,27 @@
-import BookDetail from "../components/BookDetail";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+// import BookDetail from "../components/BookDetail";
+import { getBook } from "../utils/local-data";
 
 const DetailPage = () => {
+  const { id } = useParams();
+  const [book, setBook] = useState(null);
+
+  // useEffect(() => {
+  //   const result: any = getBook(id);
+  //   setBook(result);
+  //   console.log(book);
+  // }, []);
+
   return (
     <section className="pt-28 pb-24 lg:pt-36 lg:pb-32">
       <div className="container">
         <div className="flex flex-wrap">
           <div className="w-full self-center px-4 lg:w-2/3">
             <h1 className="font-bold text-primary text-4xl lg:text-5xl">
-              Add <span className="text-secondary">Your</span> Book
+              Detail <span className="text-secondary">Page</span>
             </h1>
-            <BookDetail />
+            {/* <BookDetail book={book} /> */}
           </div>
           <div className="hidden md:block w-full self-end px-4 lg:w-1/3 ">
             <div className="mt-10 lg:right-0">
